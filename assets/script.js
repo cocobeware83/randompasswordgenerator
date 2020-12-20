b75cdd3cbb1dbb26dd20aed1bed261e850bcb97f
Coco <corycneel@gmail.com>
2020-12-20 09:09:41 -0600

    added JS to prompt for preferences on uppercase lowercase special char and numbers

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,23 @@ var confirmUpperCase;
 var confirmSpecialCharacters;
 var confirmNumbers;
 
+// initial alert window to choose length of password 
+function generatePassword() {
+  var confirmLength = (prompt("Hi! Your Password can contain between 8 - 128 characters.  Please input your preference in the form of a number."));
+
+//confirms that you have made a between 8 & 128 characters alerts you if you have made an incorrect choice and allows you to retry 
+while(confirmLength <= 7 || confirmLength >= 129) {
+  alert("You have to choose a number between 8 & 128");
+  var confirmLength = (prompt("Hi! Your Password can contain between 8 - 128 characters.  Please input your preference in the form of a number."));
+  } 
+
+// prompts user to select preferences, can choose to include uppercase, lowercase, special characters and numbers 1-9
+var confirmUpperCase = confirm("Select 'OK' if you would like to include UPPERCASE letters in your password, select 'cancel' if not.");
+var confirmLowerCase = confirm("Select 'OK' if you would like to include LOWERCASE letters in your password, select 'cancel' if not.");
+var confirmSpecialCharacters = confirm("Select 'OK' if you would like to include SPECIAL CHARACTERS in your password, select 'cancel' if not.");
+var confirmNumbers = confirm("Select 'OK' if you would like to include NUMBERS 1-9 in your password, select 'cancel' if not.");    
+
+
 
 // Write password to the #password input
 function writePassword() {
