71520a5429ea92debe12131bfb9fcd0849b017fe
Coco <corycneel@gmail.com>
2020-12-20 11:03:41 -0600

    added a loop back in case user fails to choose at least one criteria, added concats for all variations, added return function for password

--- assets/script.js
+++ assets/script.js
@@ -32,16 +32,46 @@ var confirmLowerCase = confirm("Select 'OK' if you would like to include LOWERCA
 var confirmSpecialCharacters = confirm("Select 'OK' if you would like to include SPECIAL CHARACTERS in your password, select 'cancel' if not.");
 var confirmNumbers = confirm("Select 'OK' if you would like to include NUMBERS 1-9 in your password, select 'cancel' if not.");    
 
+  //if user fails to select at least ONE parameter i.e chooses cancel every time, the prompts will loop back and allow them to try again
+  while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacters === false && confirmNumbers === false) 
+  {
+    alert("Hey wait!! Please choose at least ONE parameter");
+    var confirmUpperCase = confirm("Select OK if you would like to include UPPERCASE letters in your password, select cancel if not.");
+    var confirmLowerCase = confirm("Select OK if you would like to include LOWERCASE letters in your password, select cancel if not.")
+    var confirmSpecialCharacters = confirm("Select OK if you would like to include SPECIAL CHARACTERS in your password, select cancel if not.");   
+    var confirmNumbers = confirm("Select OK if you would like to include NUMBERS 1-9 in your password, select cancel if not.");
+} 
+
+//concats for different variations of criteria selection
+var passwordCharacters = []
+    if (confirmUpperCase) {
+      passwordCharacters = passwordCharacters.concat(alphabetUPPER)
+    }
+    if (confirmLowerCase) {
+      passwordCharacters = passwordCharacters.concat(alphabetLOWER)
+    }
+    if (confirmSpecialCharacters) {
+      passwordCharacters = passwordCharacters.concat(specialCharacters)
+    }
+    if (confirmNumbers) {
+      passwordCharacters = passwordCharacters.concat(numbers)
+    }
+    
 
 
 // Write password to the #password input
+  var randomPassword = []
+  for (var i = 0; i < confirmLength; i++) {
+  randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+}
+  return randomPassword;
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
